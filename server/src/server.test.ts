78d53ab13b11928c8f3aad8e511929d1e74e44a8
weinandt <nick.weinandt@gmail.com>
2023-01-26 08:30:23 -0600

    Express is hooked up

--- server/src/server.test.ts
+++ server/src/server.test.ts
@@ -1,9 +1,7 @@
 import { assert } from 'console'
-import { Server } from './server'
 
 describe('Server tests', () => {
     it('test', () => {
-        const server = new Server()
-        assert(server.start())
+        
     })
 })
